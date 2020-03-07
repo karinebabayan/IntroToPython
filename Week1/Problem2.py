3c4e34da8f7aa2a491147308ce553a7d41bbf5ca
User <harutyunyan.xikar@bk.ru>
2020-03-07 12:56:23 +0400

    Second Commit

---
+++ Week1/Problem2.py
@@ -0,0 +1,10 @@
+course = "Intro to Python"
+student = "Karine_Babayan"
+nickname = "Fish"
+grade = 11
+python_expert = True
+print ("variable_name:" + course)
+print ("variable_name:" + student)
+print ("variable_name:" + nickname)
+print ("variable_name:", grade)
+print ("variable_name:" ,python_expert)
---
+++ Week1/Problem4.py
@@ -0,0 +1,5 @@
+AB = 3
+AC = 4
+BC = (AB**2 + AC**2)**0.5
+BC = int (BC)
+print ("The hypotenuse of the triangle ABC = ", BC)
