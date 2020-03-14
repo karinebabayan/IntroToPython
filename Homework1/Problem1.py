0b37eaed6760e6f4216fd5bb05af4755535e128d
karinebabayan <karinebabayan12@gmail.com>
2020-03-14 22:45:59 +0400

    second commit

---
+++ Homework1/Problem1.py
@@ -0,0 +1,25 @@
+difficulty = 3
+int = difficulty
+ingredients = ["flour","butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print ("apples" in ingredients)
+print ("butter" in ingredients)
+print (("eggs" in ingredients) or ("margarine" in ingredients))
+print (("eggs" in ingredients) and ("margarine" in ingredients))
+flour = 175
+butter = 175
+sugar = '100g'
+eggs = 2
+cocoa_powder = '1ts'
+baking_powder = 0.5
+print (type (flour))
+print (type (butter))
+print (type (sugar))
+print (type (eggs))
+print (type (cocoa_powder))
+print (type (baking_powder))
+print("Flour - ", flour)
+print("Butter - ", butter)
+print("Sugar - ", sugar)
+print("Eggs - ", eggs)
+print("Cocoa powder - ", cocoa_powder)
+print("Baking powder - ", baking_powder)
