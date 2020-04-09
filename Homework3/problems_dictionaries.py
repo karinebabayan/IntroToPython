e2e5b70574040aa9cc5727a19253d8e9f30ac046
karinebabayan <karinebabayan12@gmail.com>
2020-04-09 22:58:56 +0400

    fifth commit

--- Homework3/problems_dictionaries.py
+++ Homework3/problems_dictionaries.py
@@ -1,8 +1,8 @@
 market = {'dairy': ['yogurt', 'cheese'],'fruits': ['banana', 'apple', 'orange', 'lemon', 'apple', 'banana', 'banana']}
 print('Market before changes: ', market)
 market['candies'] = ['mars', 'kinder', 'twix'] 
-market['fruits'].sort()
-print('Market sorted:', market)
+market['fruits'].sort() #market['fruits'] = sorted(list(set(market['fruits']))) 
+print('Market sorted:', market) 
 set_market = set(market['fruits'])
 market['fruits'] = list(set_market) 
 print('Market after chhanges: ', market)
