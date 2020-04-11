1db82364a12ba59ba331d1373a976a7177568ddb
karinebabayan <karinebabayan12@gmail.com>
2020-04-11 16:03:51 +0400

    seventh commit

---
+++ Week5/calc.py
@@ -0,0 +1,15 @@
+def calculate_cube(x):
+    isinstance(x,int)
+    return(x**3)
+def calculate_square(x):
+    isinstance(x,int)
+    return(x**2)
+import pretty_print
+def main():
+    result = calculate_square(2)
+    pretty_print.simple_print(result)
+    result = calculate_cube(4)
+    pretty_print.pro_print(result)
+
+if __name__ == '__main__':
+    main()
---
+++ Week5/pretty_print.py
@@ -0,0 +1,5 @@
+def simple_print(x):
+    isinstance(x,int)
+    print("Result: %s"%x)
+def pro_print(x):
+    print("The result of the operation is %s"%x)
