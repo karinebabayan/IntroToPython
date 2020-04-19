a86786a2064341372e8fe4c61c408cf081a7e6fa
karinebabayan <karinebabayan12@gmail.com>
2020-04-19 23:25:04 +0400

    8th commit

--- Homework5/Customer.py
+++ Homework5/Customer.py
@@ -1,8 +1,9 @@
 import Productcheck
 
 def buy(product, num, price):
+    pr=int(num)*int(price)
     if Productcheck.check(product,num):
-        print('You bought %s and spent %d'%(product, num*price))
+        print('You bought %s and spent %d'%(product, pr))
     else:
         print('Sorry! We are out of this product.')
 
--- Homework5/Productcheck.py
+++ Homework5/Productcheck.py
@@ -1,8 +1,8 @@
 products ={"candy": 10, "juice": 5, "pen": 50}
 
 def check(product, num):
-    
-    if (product in products.keys()) and (products.values()>=num):
+    num=int(num)
+    if (product in products.keys()) and (products[product]>=num):
         return True
     else: 
         return False
